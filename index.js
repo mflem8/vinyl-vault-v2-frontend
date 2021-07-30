c495f2169b29cf86f45e16c2b296eca8fa41ef29
mflem8 <matthewfleming8@gmail.com>
2021-07-30 17:16:14 -0400

    initial get fetch

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const app = new App()
+const baseUrl = "http://localhost:3000/albums"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getAlbums();
+});
+
+    const getAlbums = () => {
+        fetch(baseUrl)
+        .then(res => res.json())
+        .then(albums => {
+        albums.data.forEach(albums => {
+            const albumMarkup = `<div data=id=${album.id}>
+                <img src=${album.attributes.image_url}
+                height="200" width="250">
+                <h3>${album.attributes.title}</h3>
+                <h4>${album.attributes.artist}
+                <p>${album.attributes.genre.name}</p>
+                </div>`;
+
+                document.querySelector('#album-container').innerHTML += albumMarkup
+        })
+    })
+}
